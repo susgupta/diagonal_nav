25caf9bdf73054f65f2e9ea4853c11eea3b2808a
susgupta <guptask.7@gmail.com>
2018-11-12 19:33:12 -0500

    Update JS to Remove the 'Active' Handing
    Remove the active handling in js add class state.  Just straight up rotate on click.

--- js/main.js
+++ js/main.js
@@ -8,26 +8,15 @@ var hover = false;
 
 //add event listner
 menu.addEventListener("click", function() {
-  console.log("Menu clicked");
-
+  //set toggle
   openMenu = !openMenu ? true : false;
 
   nav.classList.add("menu-active");
   main.classList.add("menu-active");
-  nav.classList.remove("menu-hover");
-  main.classList.remove("menu-hover");
 
-  //call toggling
-  actionOnOpenMenu();
-});
-
-// common method for open menu toggle click
-function actionOnOpenMenu() {
+  //remove the class if toggle off
   if (!openMenu) {
-    nav.classList.add("menu-hover");
-    main.classList.add("menu-hover");
-  } else {
-    nav.classList.remove("menu-hover");
-    main.classList.remove("menu-hover");
+    nav.classList.remove("menu-active");
+    main.classList.remove("menu-active");
   }
-}
+});
